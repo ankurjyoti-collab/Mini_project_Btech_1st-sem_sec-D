67b46b886af47fdebdc9f122f6ec703d4f7d61d3
Ankur jyoti 131 <ankurjyotirajbongshi311@gmail.com>
2025-12-02 20:18:45 +0530

    Create carpenters_measurement.c

---
+++ carpenters_measurement.c
@@ -0,0 +1,56 @@
+/* Problem title: The Carpenter’s Measurement Problem
+
+Problem Story:
+A carpenter named Mohan is cutting two wooden planks of different lengths.
+He wants to cut both planks into smaller equal pieces without any leftover wood.
+To do this, he needs to find the greatest common divisor (GCD) of the two lengths.
+He also wants to know the least common multiple (LCM) — the smallest length at which both planks can be exactly aligned if placed end to end repeatedly.
+
+Can you help Mohan calculate both the GCD and LCM of the given two lengths?
+Task:
+Write a program that takes two integers as input and:
+
+Calculates the GCD (Greatest Common Divisor) of the two numbers.
+
+Calculates the LCM (Least Common Multiple) of the two numbers.
+Input Format:
+The first line contains two integers — the lengths of the two planks.
+
+Output Format:
+Print two lines:
+First line: "GCD = <value>"
+Second line: "LCM = <value>"
+Example 1:
+Input: 12 18
+Output:
+GCD = 6
+LCM = 36
+
+Example 2:
+Input: 8 20
+Output:
+GCD = 4
+LCM = 40*/
+
+//Code
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("enter 2 number \n ");
+    scanf(" %d %d", &a, &b);
+    int c = a;
+    int d = b;
+
+    while (d != 0)
+    {
+        int t = d;
+        d = c % d;
+        c = t;
+    }
+    int x=c;
+    int z = (a * b) / x;
+    printf("GCD = %d \n", x);
+    printf("LCM = %d", z);
+    return 0;
+}
