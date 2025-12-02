d13b0b91e4914721f6a49e7c2d02fe00ee20db70
Ankur jyoti 131 <ankurjyotirajbongshi311@gmail.com>
2025-12-02 20:19:47 +0530

    Create nehas_diary_entries.c

---
+++ nehas_diary_entries.c
@@ -0,0 +1,52 @@
+/*Problem title: Reverse the Array (Neha’s Diary Entries)
+🧩 Problem Story:
+
+Neha keeps a diary of her daily exercise times (in minutes).
+She wants to review her progress, but she prefers to see the latest day’s record first.
+
+Write a program to help Neha reverse the order of her recorded exercise times.
+
+✅ Task:
+
+Given n numbers representing Neha’s daily exercise time, print them in reverse order.
+
+🧠 Input Format:
+
+The first line contains an integer n — the number of days.
+
+The second line contains n space-separated integers representing exercise times.
+Output Format:
+
+Print the exercise times in reverse order on a single line separated by spaces.
+
+🧩 Example 1:
+
+Input
+5
+10 20 30 40 50
+
+Output
+50 40 30 20 10*/
+
+
+//Code
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("enter number of days: ");
+    scanf("%d", &a);
+
+    int b[a];
+    printf("enter exercise time: ");
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+
+    for (int i = a - 1; i >= 0; i--)
+    {
+        printf("%d \t", b[i]);
+    }
+    return 0;
+}
