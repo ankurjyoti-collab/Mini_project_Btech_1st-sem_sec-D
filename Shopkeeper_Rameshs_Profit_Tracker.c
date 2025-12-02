a665a1c597f7c423d1f971625e6182e6d29bab12
Ankur jyoti 131 <ankurjyotirajbongshi311@gmail.com>
2025-12-02 20:15:23 +0530

    Create Shopkeeper_Rameshs_Profit_Tracker.c

---
+++ Shopkeeper_Rameshs_Profit_Tracker.c
@@ -0,0 +1,94 @@
+
+/*Problem Title: Shopkeeper Ramesh’s Profit Tracker 💰
+🧩 Story:
+
+Ramesh owns a small grocery shop in his town.
+He keeps a record of his daily profit or loss for a week in an array.
+
+If the value is positive, it means he made a profit that day.
+
+If the value is negative, it means he had a loss that day.
+
+At the end of the week, Ramesh wants to know:
+
+His total profit (sum of all positive values)
+
+His total loss (sum of all negative values, as a positive number)
+
+His net balance (profit + loss, considering signs)
+
+Help Ramesh write a program to calculate these values.
+
+Problem Statement:
+
+Given an array records[] of size n, where each element represents the daily profit or loss,
+find and print the total profit, total loss, and net balance.
+
+Input Format:
+
+The first line contains an integer n — number of days.
+
+The second line contains n space-separated integers representing profit or loss on each day.
+
+Output Format:
+
+Print three lines:
+
+Total Profit: X
+Total Loss: Y
+Net Balance: Z
+
+
+Where:
+
+X → sum of all positive numbers
+
+Y → sum of all negative numbers converted to positive value
+
+Z → X − Y*/
+
+
+//Code
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter number of days: ");
+    scanf("%d", &a);
+
+    int b[a];
+    int x = 0, y = 0;
+
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+    for (int i = 0; i < a; i++)
+    {
+        if (b[i] > 0)
+        {
+            x += b[i];
+        }
+        else
+        {
+            y += b[i];
+        }
+    }
+    if (y < 0)
+    {
+        y = y * -1;
+    }
+
+    int z = x - y;
+    if (z < 0)
+    {
+        z = z * -1;
+    }
+
+    printf("Total Profit: %d\n", x);
+    printf("Total Loss: %d\n", y);
+    printf("Net Balance: %d\n", z);
+
+    return 0;
+}
